0bf932d59e3e341d8588143fc695477f8bb192b7
JaakkoSai <91462991+JaakkoSai@users.noreply.github.com>
2023-12-04 13:38:07 +0200

    import

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Tuotelista from "./components/Tuotelista";
 import { CssBaseline } from "@mui/material";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
