fde04ada219e7f51b229bfcefce8bdeecd7c0651
JaakkoSai <91462991+JaakkoSai@users.noreply.github.com>
2023-12-04 13:07:19 +0200

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Tuotelista from "./components/Tuotelista";
+import { CssBaseline } from "@mui/material";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+
+function App() {
+  return (
+    <>
+      <CssBaseline /> {/* Normalize the styles */}
+      <Router>
+        <Routes>
+          <Route path="/tuotteet" element={<Tuotelista />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
