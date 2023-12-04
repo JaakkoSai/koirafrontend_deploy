363dda389d078d44baf8a54072fffb5e3eff2156
JaakkoSai <91462991+JaakkoSai@users.noreply.github.com>
2023-12-04 13:28:40 +0200

    added deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/koirafrontend/",
+  base: "/koirafrontend_deploy/",
   plugins: [react()],
 });
